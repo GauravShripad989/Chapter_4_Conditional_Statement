d3c0f006ce97b166cab073c48f95a85b5148c77f
GauravShripad989 <gauravshripad127@gmail.com>
2023-10-11 06:16:14 +0530

    Recalled the conditions for if - else Statement

---
+++ Conditional_Statement.java
@@ -0,0 +1,15 @@
+package Chapter_4_Conditional_Statement.Chapter_4_Conditional_Statement;
+
+public class Conditional_Statement {
+    public static void main(String[] args) {
+        //if else (All)
+        int age = 62;
+        if(age>=18 && age <= 60){
+            System.out.println("You can Drive");
+        }else {
+            System.out.println("You cannot drive Yet");
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Chapter_4_Conditional_Statement
+<hr>
+<p>practice on conditional statements</p>
