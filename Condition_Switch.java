b72d4b9789e1ac6b1e7a99d1c944bd4430ecf337
GauravShripad989 <gauravshripad127@gmail.com>
2023-10-11 06:35:22 +0530

    all patterns of if else statement

---
+++ Condition_Switch.java
@@ -0,0 +1,7 @@
+package Chapter_4_Conditional_Statement.Chapter_4_Conditional_Statement;
+
+public class Condition_Switch {
+    public static void main(String[] args) {
+
+    }
+}
--- Conditional_Statement.java
+++ Conditional_Statement.java
@@ -1,5 +1,5 @@
 package Chapter_4_Conditional_Statement.Chapter_4_Conditional_Statement;
-
+import java.util.Scanner;
 public class Conditional_Statement {
     public static void main(String[] args) {
         //if else (All)
@@ -8,6 +8,40 @@ public class Conditional_Statement {
             System.out.println("You can Drive");
         }else {
             System.out.println("You cannot drive Yet");
+        }
+
+
+
+        // If - else ladder
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int ageInt = sc.nextInt();
+        if(ageInt >= 60){
+            System.out.println("You are too experienced");
+        } else if (ageInt >= 40) {
+            System.out.println("You are experienced");
+        } else if (ageInt >= 21) {
+            System.out.println("You are fresher");
+        } else if (ageInt >= 18) {
+            System.out.println("Finish your education first");
+        } else if(ageInt < 18){
+            System.out.println("Are you still a Baby ?");
+        }
+
+        //nested if
+        System.out.println("Enter your age: ");
+        int ageSub = sc.nextInt();
+        System.out.println("Enter your Sub: ");
+        int Sub = sc.nextInt();
+        if(ageSub >= 18){
+            if (Sub >= 1000){
+                System.out.println("Lucky you have "+ Sub + " at the age of "+ageSub);
+            } else {
+                System.out.println("Keep it up");
+            }
+        } else {
+            System.out.println("Start it when time is write");
         }
 
 
